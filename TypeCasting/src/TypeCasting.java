eec05f4a9a61e6dc5cadadb55aa3be75e51333fb
MohammedMaisoon <159471745+MohammedMaisoon@users.noreply.github.com>
2024-07-28 18:44:51 +0530

    Add files via upload

---
+++ TypeCasting/src/TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a; // implicit type casting
+
+        double c = 10.5;
+        int d = (int) a; // explicit type casting
+        System.out.println(a+d);
+
+    }
+}
